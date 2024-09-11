9685234f9c47874c898b9e21ca38f640e0eb27bc
Said Masih <saidmsajady@gmail.com>
2024-09-10 17:50:05 -0700

    Progress on Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Countries from './pages/Countries/Countries'
 import Create from './pages/Create/Create'
 import Home from './pages/Home/Home'
 import Trips from './pages/Trips/Trips'
+import Contact from './pages/Contact/Contact'
 
 import NotFound from './pages/NotFound'
 
@@ -22,6 +23,7 @@ function App() {
         <Route path='/Trips' element={<Trips />} />
         <Route path='/Create' element={<Create />} />
         <Route path='/Countries' element={<Countries />} />
+        <Route path='/Contact' element={<Contact />} />
         <Route path={'*'} element={<NotFound />}/>
       </Routes>
       
